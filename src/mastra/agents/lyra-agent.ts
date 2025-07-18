8213504873dff5191189a4d8737dc5ad4578de27
MeiLiing <13270883+k2rbpz@users.noreply.github.com>
2025-07-18 16:19:31 +0300

    lyra is born (with no tools)

--- src/mastra/agents/lyra-agent.ts
+++ src/mastra/agents/lyra-agent.ts
@@ -2,7 +2,6 @@ import { google } from '@ai-sdk/google';
 import { Agent } from '@mastra/core/agent';
 import { Memory } from '@mastra/memory';
 import { LibSQLStore } from '@mastra/libsql';
-import { coingeckoTool } from '../tools/coingecko-tool';
 import { mcp } from '../mcp-client';
 import { lyraInstructions } from './lyra-instructions';
 
@@ -10,8 +9,8 @@ export const lyraAgent = new Agent({
   name: 'Lyra',
   // You can easily switch between personas here, e.g., lyraInstructions.kebabShop
   instructions: lyraInstructions.archivistOfTheEther,
-  model: google('gemini-1.5-flash'),
-  tools: { coingeckoTool, ...await mcp.getTools({ server: "coingecko" }) }, // Include local and external tools
+  model: google('gemini-2.5-flash'),
+  // tools: { ...await mcp.getTools() }, // Include local and external tools
   memory: new Memory({
     storage: new LibSQLStore({
       url: 'file:../mastra.db', // path is relative to the .mastra/output directory
